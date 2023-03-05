830ce9a73e504df2ba818de9fb93dfd1dcee0337
Misael Alberto <misael.alberto@outlook.com>
2023-03-05 12:23:49 -0300

    corrigindo alteração para mysql8

--- config/db.js
+++ config/db.js
@@ -1,10 +1,9 @@
 /* --  Usando mysql 5.3 ---*/
-//const mysql = require('mysql');
-//connectionString = process.env.DATABASE_URL 
+// const mysql = require('mysql');
+// connectionString = process.env.DATABASE_URL 
 
 /* --  Usando mysql 8 ---*/
 const mysql = require('mysql2');
-
 
 db = {};
 db.cnn = {};
@@ -16,12 +15,16 @@ db.cnn.exec = function(query, callback){
         password: process.env.PASSWORD,
         database: process.env.DATABASE 
     });
+    /* --  fim mysql 8 ---*/
+
     /* --  Usando mysql 5 ---*/
-    // var connection = mysql.createConnection(connectionString);
-    // connection.query(query, function(err, rows){
-    //     callback(rows, err);
-    //     connection.end();
-    // });
+    //var connection = mysql.createConnection(connectionString);
+    /* --  fim mysql 5 ---*/
+
+    connection.query(query, function(err, rows){
+        callback(rows, err);
+        connection.end();
+    });
 };
 
 module.exports = db;
